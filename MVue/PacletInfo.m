1dbb3caa14392df879c687b533beae763e094a72
Kuba Podkalicki <kuba.pod@gmail.com>
2018-05-17 22:04:35 +0200

    fix(dev) wl package structure

---
+++ MVue/PacletInfo.m
@@ -0,0 +1,9 @@
+
+Paclet[
+	Name -> "MVue", 
+	Version -> "0.0.1", 
+	MathematicaVersion -> "11.2+", 
+	Description -> "", 
+	Creator -> "Jakub (Kuba) Podkalicki",  
+	Extensions -> {{"Kernel", Context -> "MVue`"}}
+]
