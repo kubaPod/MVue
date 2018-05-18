46882f58a6947a3b739cc7e5721d6eb654507e95
kuba <kuba@gluonvision.com>
2018-05-18 12:13:45 +0200

    fix(dev) .m init; tests init

---
+++ MVue/MVue.m
@@ -0,0 +1,61 @@
+(* ::Package:: *)
+
+(* Author: Kuba Podkalicki *)
+(* Notes:
+     TODO:
+       - option: content type png/html
+       - option: continuous action   
+   
+       - dependency tree for body and dynamic structure at the end.
+       
+     short scheme:
+       Manipulate[body, {var_i, __}..]    ----> VManipulate[<|"controllers" \[Rule] dataset_, "body" \[Rule] function_|>]
+       
+       CloudDeploy[VManipulate] --\[Rule] (body \[Rule] apifunction) + (v-manipulate-templ + controllers --\[Rule] app html)
+*)
+
+
+
+BeginPackage["MVue`"];
+
+
+ClearAll["`*", "`*`*"]
+
+
+MVue::usage = "MVue is a constructor for V* objects. V* objects are meant for special deployment procedure.";
+
+
+Begin["`Private`"];
+
+
+MVue::argpatt = "Currently MVue only recognizes Manipulate[body_, varSpec:({var_Symbol, __}|{{var_Symbol, __}, __} ) ..]";
+
+
+
+MVue[m:Verbatim[Manipulate][body_, varSpec:({_Symbol, __}|{{_Symbol, __}, __} ) ..], OptionsPattern[]]:=Module[
+  { vars , temp}
+, ManipulateBlock[{varSpec},
+    temp = {varSpec};
+    Hold[#]&[temp]
+  ]
+];
+
+MVue[opts:OptionsPattern]:=Function[expr, MVue[expr, opts]];  
+
+MVue[___]:=(Message[MVue::argpatt]; $Failed)
+
+
+
+ManipulateBlock::usage = "ManipulateBlock[{varSpec}, expr] acts like Block[{x1, x2}, expr] "<>
+  "where xi are extracted from varSpec which should follow a basic syntax for Manipulate's  controllers specification.";
+ManipulateBlock // Attributes = HoldAll;
+ManipulateBlock[{varSpec:({_Symbol, __}|{{_Symbol, __}, __} )..}, expr_]:=Module[
+  {vars}
+, vars = Hold[varSpec] /. { {{s_Symbol, __}, __}:> s, {s_Symbol, __}:>s}
+; vars /. Hold[vars__]:> Block[{vars}, expr ]
+]
+
+
+End[];
+
+EndPackage[];
