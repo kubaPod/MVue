54e46ff8b9fc4dc7f8aa8b0ec95b301d56cb426e
Kuba <kuba.pod@gmail.com>
2018-05-28 09:05:09 +0200

    version-increment

--- MVue/PacletInfo.m
+++ MVue/PacletInfo.m
@@ -1,9 +1,4 @@
-
-Paclet[
-	Name -> "MVue", 
-	Version -> "0.0.3",
-	MathematicaVersion -> "10.4+",
-	Description -> "", 
-	Creator -> "Jakub (Kuba) Podkalicki",  
-	Extensions -> {{"Kernel", Context -> "MVue`"}}
-]
+(* Created with the Wolfram Language : www.wolfram.com *)
+Paclet[Name -> "MVue", Version -> "0.1.0", MathematicaVersion -> "10.4+", 
+ Description -> "", Creator -> "Jakub (Kuba) Podkalicki", 
+ Extensions -> {{"Kernel", Context -> "MVue`"}}]
